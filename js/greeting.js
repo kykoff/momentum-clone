636fc16f20f4f46a260073fcf6d4422d07e6c6f5
kykoff <fwestaa@gmail.com>
2022-07-31 21:11:41 +0900

    Create weather.js, Modify greeting.js, css files

--- js/greeting.js
+++ js/greeting.js
@@ -1,15 +1,26 @@
 // DOM
 const title = document.querySelector(".title");
 const greeting = document.querySelector(".title__greeting");
+const mustTitleBox = document.querySelector(".must-form__title");
+const must = document.querySelector(".must");
+const mustTitle = document.querySelector(".must-title");
+const email = document.querySelector(".user-email");
+const footer = document.querySelector("footer");
+const mustFormBox = document.querySelector(".must-form__input");
 
-const subForm = document.querySelector(".sub-form");
+
 const nameForm = document.querySelector(".name-form");
 const emailForm = document.querySelector(".email-form");
 const passwordForm = document.querySelector(".password-form");
+const mustForm = document.querySelector(".must-form");
+
+
 
 const nameInput = document.querySelector(".name-form input");
 const emailInput = document.querySelector(".email-form input");
 const passwordInput = document.querySelector(".password-form input");
+const mustFormInput = document.querySelector(".must-form__input input");
+
 
 // global variables
 const HIDDEN = "hidden";
@@ -30,6 +41,8 @@ function emailSubmitHandler(e) {
     emailInput.value="";
     emailForm.classList.add(HIDDEN);
     passwordForm.classList.remove(HIDDEN);
+    const span = document.createElement("span");
+    span.innerText = emailInput.value;
 }
 
 
@@ -38,6 +51,8 @@ function passwordSubmitHandler(e) {
     localStorage.setItem("password", passwordInput.value);
     passwordInput.value="";
     passwordForm.classList.add(HIDDEN);
+    mustForm.classList.remove(HIDDEN);
+    footer.classList.remove(HIDDEN);
     paintTitle();
 }
 
@@ -45,7 +60,7 @@ function passwordSubmitHandler(e) {
 function paintTitle() {
     const userName = localStorage.getItem("name");
     title.classList.remove(HIDDEN);
-    subForm.classList.remove(HIDDEN);
+    mustForm.classList.remove(HIDDEN);
 
     const hour = new Date().getHours();
     if (hour >= 5 && hour < 12) {
@@ -70,6 +85,7 @@ if (savedUserName !== null && savedUserEmail !== null && savedUserPassword !== n
     nameForm.classList.add(HIDDEN);
     emailForm.classList.add(HIDDEN);
     passwordForm.classList.add(HIDDEN);
+    footer.classList.remove(HIDDEN);
     paintTitle();
 } else {
     title.classList.add(HIDDEN);
@@ -81,4 +97,51 @@ if (savedUserName !== null && savedUserEmail !== null && savedUserPassword !== n
 // 모든 정보가 다 있을 때만 title이 표시되도록
 // 하나라도 없으면 그냥 처음부터 하도록 
 
+function saveMustDo(mustDo) {
+    localStorage.setItem("must", mustDo);
+}
 
+function deleteMustDo(e) {
+    const h1 = e.target.parentElement;
+    h1.remove();
+    localStorage.removeItem("must");
+    mustFormBox.classList.remove(HIDDEN);
+    mustTitleBox.classList.add(HIDDEN);
+}
+
+function paintMustDo(mustDo) {
+    const h1 = document.createElement("h1");
+    const x = document.createElement("button");
+    h1.innerText = mustDo;
+    x.innerText = "x";
+    x.addEventListener("click", deleteMustDo);
+    must.appendChild(h1);
+    h1.appendChild(x);
+}
+
+function mustFormHandler(e) {
+    e.preventDefault();
+    mustFormBox.classList.add(HIDDEN);
+    mustTitleBox.classList.remove(HIDDEN);
+    const mustDo = mustFormInput.value; 
+    mustFormInput.value="";
+    paintMustDo(mustDo);
+    saveMustDo(mustDo);
+}
+
+mustForm.addEventListener("submit", mustFormHandler);
+
+const getMustDo = localStorage.getItem("must");
+
+if (getMustDo !== null) {
+    mustFormBox.classList.add(HIDDEN);
+    mustTitleBox.classList.remove(HIDDEN);
+    paintMustDo(getMustDo);
+}
+
+
+// 좀 더 깔끔하게 클래스 정리
+// 새로고침 하지 않아도 must do 뜨도록
+// email
+// weather
+// geolocation
